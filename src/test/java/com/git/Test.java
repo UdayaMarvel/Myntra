02798d541904408b4164b77db5883d2bb6406a96
Kaviya@0801 <kaviyaanand08@gmail.com>
2025-03-06 21:14:05 +0530

    text

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -2,17 +2,11 @@ package com.git;
 
 public class Test {
 public static void main(String[] args) {
+	System.out.println("Test");
+
 	
 	System.out.println("kavi");
-	System.out.println("galaxy");
-	System.out.println("star");
 	System.out.println("moon");
-	
-	System.out.println("Test");
-	System.out.println("ajith");
-	System.out.println("1234");
-	System.out.println("pass");
-	System.out.println("Fail");
-	System.out.println("hello");
+	System.out.println("galaxy");
 }
 }
