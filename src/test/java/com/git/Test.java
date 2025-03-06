56e06750d4a06eb4a1af6f8d410a4df884d8d0ae
Sandy799342 <santhoshkmr799@gmail.com>
2025-03-06 23:26:00 +0530

    Update Test.java

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -4,7 +4,9 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("Test");
 	System.out.println("test 1");
-	System.out.println("test 2");	
+	System.out.println("test 2");
+	System.out.println("test 3");	
+	
 	
 	
 
