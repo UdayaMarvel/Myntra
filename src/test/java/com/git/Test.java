549b538bd8a1f3e3ff10a8c392159496dde63c37
Santhosh799342 <santhoshkmr799@gmail.com>
2025-03-06 21:31:42 +0530

    updated one

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -4,8 +4,7 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("Test");
 	System.out.println("test 1");
-	System.out.println("tset 2");
-	dt
+	System.out.println("test 2");	
 	
 	
 
