2c04711665d5b63bed095dd6b0a3664e6217bae1
Dhamudharan-10 <dhamodharan10@gmail.com>
2025-03-06 15:25:25 +0530

    updated

--- src/test/java/JavaProgram/Palindrome.java
+++ src/test/java/JavaProgram/Palindrome.java
@@ -7,14 +7,14 @@ public class Palindrome {
 		Scanner admin = new Scanner (System.in);
 		System.out.print("Enter the name: ");
 	       String name = admin.nextLine();
-	       String rev = "";
+	       String rev1 = "";
 	       for(int i=name.length()-1; i>=0; i--) {
 	    	  char letters = name.charAt(i);
-	    	   rev=rev+letters;
+	    	   rev1=rev1+letters;
 	       }
 	       System.out.println("Name of the person: "+name);
-	       System.out.println("rev of the name: "+rev);
-	       if (name.equalsIgnoreCase(rev)) {
+	       System.out.println("rev of the name: "+rev1);
+	       if (name.equalsIgnoreCase(rev1)) {
 	    	   System.out.println("It is the palindrome: "+name);
 	       } else {
 	    	   System.out.println("It is not a palindrome: "+name);
