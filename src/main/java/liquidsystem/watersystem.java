dd0bb859572149865e23a8e4d1879487bea6352b
unknown <ajithkarthi158@gmail.com>
2025-03-06 16:04:49 +0530

    First change

--- src/main/java/liquidsystem/watersystem.java
+++ src/main/java/liquidsystem/watersystem.java
@@ -1,5 +1,45 @@
 package liquidsystem;
 
+import Watertank.watersupply.Watertank;
+
 public class watersystem {
 
+	public class Watertank {
+		int a= 2000;
+
+		public void mainTank() {
+		 a= 5000;
+		int a= 10;	
+		System.out.println("Tank one=>" + this.a);
+		}
+
+		public void districtTank() {
+		int a= 100;	 
+		System.out.println("Tank Two=>" + a);
+
+		}
+		public  void corporationTank() {
+
+			int a= 1000;
+		System.out.println("Tank Three=>" + this.a);
+		}
+
+		public void villageTank() {
+			
+		System.out.println("Tank Foru=>" +a);
+			
+		}
+		public void main(String[] args) {
+			
+			Watertank s = new Watertank();
+			
+			s.mainTank();
+			s.districtTank();
+			s.corporationTank();
+			s.villageTank();
+			System.out.println(a);
+			System.out.println("Hi");
+		}
+	}
 }
+
