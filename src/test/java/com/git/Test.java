91f084b9929c7d7588420c1c85b4de13425d795c
SakthiKannan-Qa <sakthika2001@gmail.com>
2025-03-06 13:52:24 +0530

    Few lines added

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -3,5 +3,9 @@ package com.git;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Test");
+	System.out.println("sakthi");
+	System.out.println("12345");
+	System.out.println("false");
+	System.out.println("True");
 }
 }
