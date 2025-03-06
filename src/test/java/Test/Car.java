dc23d76b08eef782f626e65cdec252d8ddd168ef
vino <associatesavg290@gmail.com>
2025-03-07 00:10:28 +0530

    new method

--- src/test/java/Test/Car.java
+++ src/test/java/Test/Car.java
@@ -4,24 +4,26 @@ package Test;
 import java.util.Scanner;
 
 public class Car {
-	
+	int year =2025;
 	String make;
     String model;
-    public Car(String make, String model) {
+    public Car(String make, String model, int year) {
         this.make = make;
         this.model = model;
+        this.year=year;
     }
 
     
         public void displayCarDetails() {
         System.out.println("Car Make " + make);
         System.out.println("Car Model " + model);
+        System.out.println("Manufacturing year" + year);
     }
 
 
 
     public static void main(String[] args) {
-        Car myCar = new Car("Toyota", "Corolla");
+        Car myCar = new Car("Toyota", "Corolla",id);
         myCar.displayCarDetails();
     }
    
--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -1,7 +1,12 @@
 package com.git;
 
 public class Test {
+	
+	public void method() {
+		System.out.println("dummy");
+	}
 public static void main(String[] args) {
 	System.out.println("Test");
+	
 }
 }
