9ea462092b4de747341ec0165cedc96fd8549f42
Kaviya@0801 <kaviyaanand08@gmail.com>
2025-03-06 19:27:38 +0530

    new project

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -2,6 +2,12 @@ package com.git;
 
 public class Test {
 public static void main(String[] args) {
+	
+	System.out.println("kavi");
+	System.out.println("galaxy");
+	System.out.println("star");
+	System.out.println("moon");
+	
 	System.out.println("Test");
 }
 }
