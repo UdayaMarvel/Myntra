5c9231826c65c8ef74a182a44bb31f64480d3207
RajaStark09 <rajavarma.rs7@gmail.com>
2025-03-06 20:59:40 +0530

    JIRA 0011

---
+++ src/test/java/PracticalSession/PractiseOne.java
@@ -0,0 +1,28 @@
+package PracticalSession;
+
+public class PractiseOne {
+	public static int[] removeDuplicates(int[] input){
+		int j = 0;
+		int i = 1;
+
+		if(input.length < 2){
+		return input;
+		}
+		while(i < input.length){
+		if(input[i] == input[j]){ i++;
+		}else{
+		input[++j] = input[i++];
+		}
+		}
+		int[] output = new int[j+1];
+		for(int k=0; k<output.length; k++){ output[k] = input[k];
+		}
+		return output;
+		}
+		public static void main(String a[]){
+		int[] input1 = {2,3,6,6,8,9,10,10,10,12,12};
+		int[] output = removeDuplicates(input1);
+		for(int i:output){ System.out.print(i+" ");
+		}
+		}
+}
