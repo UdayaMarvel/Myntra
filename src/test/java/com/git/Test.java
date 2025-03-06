9af30d669b2ea81516e03da815b35e167ae83f9a
aji941999 <aji99thkumar@gmail.com>
2025-03-06 19:33:01 +0530

    new update

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -3,7 +3,7 @@ package com.git;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Test");
-	System.out.println("Sakthi");
+	System.out.println("ajith");
 	System.out.println("1234");
 	System.out.println("pass");
 	System.out.println("Fail");
