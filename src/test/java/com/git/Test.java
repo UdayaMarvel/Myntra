440d49b7270890ae12a12488bdc4d5037f712600
UdayaMarvel <udayamarvel@gmail.com>
2025-03-06 12:15:09 +0530

    test Runner

--- src/test/java/com/git/Test.java
+++ src/test/java/com/git/Test.java
@@ -1,8 +1,21 @@
 package com.git;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features=".\\src\\test\\resources",
+					glue="com.myntra.men_stepdefinition",
+					plugin={"html:target","json:target/reports.json"},
+					
+					dryRun=false,
+					monochrome=true)
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Test");
 	System.out.println("New Test");
+	
 }
 }
