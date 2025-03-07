daf1b00bfdd3a90ef7f3b846bf5bc83b96e9191d
Dhamudharan-10 <dhamodharanbabu10@gmail.com>
2025-03-07 20:36:48 +0530

    changes

--- src/test/java/JavaProgram/SecondHighest.java
+++ src/test/java/JavaProgram/SecondHighest.java
@@ -2,11 +2,11 @@ package JavaProgram;
 
 public class SecondHighest {
 	public static void main(String[] args) {
-		int a[]= {10,11,19,97,17,12,18,98};
+		int a[]= {10,11,97,1,12,96};  //1 10 11 12 96 97
 		int b;
-		for(int i=0; i<a.length; i++) {
-			for(int j=i; j<a.length; j++) {
-				if(a[i] >= a[j]) {
+		for(int i=0; i < a.length; i++) { // i = 6 i < 6
+			for(int j=i; j < a.length; j++) { // j = 6 j < 6
+				if(a[i] > a[j]) { //10 > 11 
 					b = a[i];
 					a[i] = a[j];
 					a[j] = b;
