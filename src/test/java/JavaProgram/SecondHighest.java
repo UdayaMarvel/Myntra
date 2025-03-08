f2b68113c8dc738d8d2991fcf22c2e8ae40e66c6
Dhamudharan-10 <dhamodharanbabu10@gmail.com>
2025-03-08 11:28:49 +0530

    SecondHighest

--- src/test/java/JavaProgram/SecondHighest.java
+++ src/test/java/JavaProgram/SecondHighest.java
@@ -2,17 +2,17 @@ package JavaProgram;
 
 public class SecondHighest {
 	public static void main(String[] args) {
-		int a[]= {10,11,97,1,12,96};  //1 10 11 12 96 97
+		int a[]= {10,11,97,1,12,96};  //1 10 11 12 97 96
 		int b;
-		for(int i=0; i < a.length; i++) { // i = 6 i < 6
-			for(int j=i; j < a.length; j++) { // j = 6 j < 6
-				if(a[i] > a[j]) { //10 > 11 
-					b = a[i];
-					a[i] = a[j];
-					a[j] = b;
+		for(int i=0; i < a.length; i++) { // i = 6 6 < 6 false
+			for(int j=i+1; j < a.length; j++) { // j = 6 6 < 6 false
+				if(a[i] > a[j]) { //97 > 97 false
+					b = a[i]; 
+					a[i] = a[j]; 
+					a[j] = b; 
 				}
 			}
-			System.out.println("Ascending order is : "+a[i]);
+			System.out.println("Ascending order is : "+a[i]); //1 10 11 12 96 97
 		}
 		System.out.println();
 		System.out.println("Second Highest value is: "+a[a.length-2]);
